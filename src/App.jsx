2eefaead411f47a6feb86f66b31fed9433253f7a
Evileudo Matias <evileudo@gmail.com>
2025-05-22 16:09:42 -0300

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-
-import Contador from './components/Contador/Contador';
+import { useState, useEffect } from "react";
+import './App.css';
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      console.log('Executou depois de 2 segundos');
+    }, 2000);
+
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [count]);
+
+  function counter() {
+    setCount(count +1);
+  }
+
   return (
     <>
-    <Contador/>
+    <div>
+      <span>{count}</span>
+      <button onClick={counter}>COUNTER</button>
+    </div>
     </>
   )
 }
